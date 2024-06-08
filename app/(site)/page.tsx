556970f41081b70d93923b1e677a8c48c7b5e719
Dúdú Vera Cruz <130259216+SrVeraCruz@users.noreply.github.com>
2024-06-08 04:15:28 +0100

    Login/Register page - Setup prisma «Mongodb»

---
+++ app/(site)/page.tsx
@@ -0,0 +1,28 @@
+import AuthForm from "./components/authForm/AuthForm";
+
+export default function Home() {
+
+  return (
+    <div 
+      className="flex min-h-full flex-col justify-center 
+      py-12 bg-gray-100
+      sm:px-6 lg:px-8"
+    >
+      <div 
+        className="sm:mx-auto sm:w-full sm:max-w-md"
+      >
+        <img 
+          src="/logo.png" 
+          alt="logo" 
+          className="mx-auto w-20 h-20" 
+        />
+        <h2 
+          className="text-center text-3xl font-bold tracking-tight text-gray-900"
+        >
+          Sign in to your account
+        </h2>
+      </div>
+      <AuthForm />
+    </div>
+  );
+}
