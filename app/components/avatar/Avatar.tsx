c2ba04faacc058f1dfe81ccccc7350a69c545694
Dúdú Vera Cruz <130259216+SrVeraCruz@users.noreply.github.com>
2024-06-16 00:17:01 +0100

    feat: Profile Drawer

--- app/components/avatar/Avatar.tsx
+++ app/components/avatar/Avatar.tsx
@@ -1,19 +1,25 @@
 import { User } from "@prisma/client"
+import clsx from "clsx"
 import Image from "next/image"
 
 interface AvatarProps {
-  user: User
+  user: User,
+  big?: boolean
 }
 
 export default function Avatar({
-  user
+  user,
+  big
 }: AvatarProps) {
   return (
     <div className="relative">
       <div
-        className="relative inline-block rounded-full 
-          overflow-hidden size-9 md:size-11
-        "
+        className={clsx(`
+          relative inline-block rounded-full 
+          overflow-hidden 
+        `,
+          big ? 'size-[5rem] md:size-[6rem]' : 'size-9 md:size-11'
+        )}
       >
         <Image 
           src={user?.image || '/images/placeholder-user.jpg'}
@@ -24,9 +30,13 @@ export default function Avatar({
         />
       </div>
       <span 
-        className="absolute top-0 right-0 block rounded-full 
-          ring-2 ring-white bg-green-500 size-2 md:size-3
-        "
+        className={clsx(`
+          absolute block rounded-full 
+          ring-2 ring-white bg-green-500  
+        `,
+          big ? 'size-3 md:size-4 md:top-1 md:right-1 top-1.5 right-1.5' 
+              : 'size-2 md:size-[0.6rem] top-0 right-0'
+        )}
       />
     </div>
   )
